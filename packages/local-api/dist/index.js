8cf0d28592263212572a0bde0c4925c47300d41b
UmbrellaOsJohnson <umbrellaosjohnson@gmail.com>
2023-12-14 15:36:02 -0500

    worked on servce.ts try anc catch err

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,9 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
 const serve = (port, filename, dir) => {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetch cells from', filename);
-    console.log('that file is in dir', dir);
+    const app = (0, express_1.default)();
+    // to make our own async to handle errors in express to catch at serve.ts CLIin
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
